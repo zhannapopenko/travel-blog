696dc7e1447b1476506b9e266de37c82e7607eee
Zhanna Popenko <zpopenko7@gmail.com>
2023-12-22 18:15:18 -0800

    Add responsive menu icon and update mobile navigation layout

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,40 +1,70 @@
-import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBars,
+  faLocationDot,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./Header.scss";
 
 const Header = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
   return (
     <header>
       <div className="header-container">
-        <NavLink to="/" className="logo-container">
-          <span className="logo">TRAVEL</span>
+        <NavLink to="/" className="logo-container" onClick={closeMobileMenu}>
+          <span className="logo">VOYAGE</span>
           <FontAwesomeIcon icon={faLocationDot} size="xl" color="white" />
         </NavLink>
 
         <nav>
-          <ul>
-            <li>
-              <NavLink to="/" className="link">
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <NavLink to="/" className="nav-link" onClick={closeMobileMenu}>
                 Home
               </NavLink>
             </li>
-            <li>
-              <NavLink to="about" className="link">
+            <li className="nav-item">
+              <NavLink
+                to="about"
+                className="nav-link"
+                onClick={closeMobileMenu}
+              >
                 About
               </NavLink>
             </li>
-            <li>
-              <NavLink to="contact" className="link">
+            <li className="nav-item">
+              <NavLink
+                to="contact"
+                className="nav-link"
+                onClick={closeMobileMenu}
+              >
                 Contact
               </NavLink>
             </li>
             <li>
-              <NavLink to="/sign-up" className="sign-up">
-                Sign Up
+              <NavLink
+                to="sign-up"
+                className="sign-up-link"
+                onClick={closeMobileMenu}
+              >
+                <button className="sign-up-button">Sign Up</button>
               </NavLink>
             </li>
           </ul>
+
+          <div className="mobile-menu-icon" onClick={handleClick}>
+            <FontAwesomeIcon
+              icon={click ? faXmark : faBars}
+              color="white"
+              size="xl"
+            />
+          </div>
         </nav>
       </div>
     </header>
